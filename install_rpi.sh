01dd9c63dd10b580438d6e45a642ee1a622f4141
Michael Lan <al1714241@gmail.com>
2024-10-11 21:30:23 +0800

    Update install_rpi.sh

--- install_rpi.sh
+++ install_rpi.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Bash script
+
 echo "這可能要花費些時間，請耐心等待。"
 
 # Detect Raspberry Pi OS Architecture
@@ -21,56 +24,40 @@ else
     echo "跳過更新"
 fi
 
-if [ "$architecture" == "armv7l" ]; then
-    # 若是 Raspberry Pi OS (Buster) 32-bit (armv7l)
-    if [ "$version" == "11" ]; then
-        echo "錯誤: Mediapipe 目前確認只能在以下作業系統版本或架構執行:"
-        echo "Raspberry Pi OS 10 (Buster) 32-bit (armv7l)"
-        echo "Raspberry Pi OS 11 (Bullseye) 64-bit (aarch64)"
-        echo "可以去 Raspberry Pi OS 官方網站利用 Raspberry Pi Imager 下載並安裝 Raspberry Pi OS (Legacy) 或 Raspberry OS (64-bit)"
-        echo "Raspberry Pi OS 官方網站: https://www.raspberrypi.com/software/"
-        exit 1
-    elif [ "$version" == "10" ]; then
-        echo "已確認版本為 $version ($codename) 繼續進行安裝..."
-    fi
-elif [ "$architecture" == "aarch64" ]; then
-        echo "已確認架構為 $architecture 繼續進行安裝..."
-else
-    echo "警告: 不支援或無法確定是否支援的架構 ($architecture)"
-    read -p "繼續執行安裝指令檔? (按 Enter 繼續安裝，或輸入 'n' 退出): " continue_execution
-    if [ "$continue_execution" != "n" ]; then
-        echo "繼續進行安裝套件..."
-    else
-        echo "結束指令檔案"
-        exit 1
-    fi
-fi
+# 先安裝和升級必要的依賴項
+# 升級 Numpy
+echo "正在升級 Numpy..."
+pip3 install numpy
 
-# Install loguru
-pip3 install loguru
-
-# Install OpenCV & dependencies
+# 安裝 OpenCV 及其依賴項
+echo "正在安裝 OpenCV 及其依賴項..."
 sudo apt install -y libatlas-base-dev
-pip3 install opencv-python==4.6.0.*
+pip3 install opencv-python
 
-# Upgrade Numpy
-pip3 install numpy==1.21.6
+# 根據架構選擇安裝 Mediapipe 的方式
+if [ "$architecture" = "armv7l" ]; then
+    echo "已確認架構為 $architecture，開始安裝 Mediapipe..."
 
-# Install Mediapipe
-if [ "$architecture" == "armv7l" ]; then
-    if [ "$(pi_model | awk '{print $3}')" = "3" ]; then
+    # 判斷是樹莓派3還是4
+    if [ "$(echo $pi_model | awk '{print $3}')" = "3" ];then
         pip3 install mediapipe-rpi3
     else
         pip3 install mediapipe-rpi4
     fi
+
+elif [ "$architecture" = "aarch64" ]; then
+    echo "已確認架構為 $architecture，開始安裝 Mediapipe..."
+    pip3 install mediapipe
 else
+    echo "警告: 未知架構 ($architecture)，將嘗試安裝 Mediapipe..."
     pip3 install mediapipe
 fi
 
-# Downgrade protobuf to 3.20.*
-pip3 install protobuf==3.20.*
+# 最後安裝 loguru 和 luma.oled
+echo "正在安裝 loguru..."
+pip3 install loguru
 
-# Install OLED Package
+echo "正在安裝 OLED Package..."
 pip3 install luma.oled
 
 echo "安裝完成！"
